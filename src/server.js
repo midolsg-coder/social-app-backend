03277e08a5146cd1c38b60332ee2da84c61b6219
social-app-backend <midolsg@gmail.com>
2026-02-15 16:37:01 +0300

    fix console log syntax

--- src/server.js
+++ src/server.js
@@ -20,6 +20,8 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB подключена"))
   .catch(err => console.log(err));
 
-app.listen(5000, () => {
-  console.log('Сервер запущен на порту 5000');
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
 });
