45301012d25be76c16f01864b4988aa22476aa4e
social-app-backend <midolsg@gmail.com>
2026-02-17 17:22:13 +0300

    fix render port binding

--- src/server.js
+++ src/server.js
@@ -60,6 +60,6 @@ app.get("/", (req, res) => {
 // ======================
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`🚀 Сервер запущен на порту ${PORT}`);
 });
